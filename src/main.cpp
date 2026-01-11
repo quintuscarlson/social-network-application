ec1fb1becafd55218308da0f8608a8b2ea1a29a4
Quintus (Quin) Carlson <quintuscarlson@gmail.com>
2026-01-11 15:45:10 -0800

    Initialize QApplication and show SocialNetworkWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include "socialnetworkwindow.h"
 
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    SocialNetworkWindow w;
+    w.show();
+    return a.exec();
+}
