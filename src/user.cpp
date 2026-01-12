f269c1983b6c9a2a23cbe8c310436e86e408de72
Quintus (Quin) Carlson <quintuscarlson@gmail.com>
2026-01-11 16:13:32 -0800

    Refactor User class methods for clarity and efficiency

--- src/user.cpp
+++ src/user.cpp
@@ -3,119 +3,97 @@
 
 User::User(){}
 
+// Constructor to initialize a User with ID, name, year, zip, and friends
 User::User(int id, std::string name, int year, int zip, std::set<int> friends){
-	id_ = id;
-	name_ = name;
-	year_ = year;
-	zip_ = zip;
-	friends_ = friends;
-
-	}
+    id_ = id;
+    name_ = name;
+    year_ = year;
+    zip_ = zip;
+    friends_ = friends;
+}
 
 int User::getId(){
-	return id_;
+    return id_;
 }
 
 std::string User::getName(){
-	return name_;
+    return name_;
 }
 
 int User::getYear(){
-	return year_;
+    return year_;
 }
 
 int User::getZip(){
-	return zip_;
+    return zip_;
 }
 
+// Returns the set of friend IDs
 std::set<int>& User::getFriends(){
-		return friends_;
-	}
+    return friends_;
+}
 
+// Adds a friend if they aren't already in the friends set
 void User::addFriend(int id){
+    for(int val : friends_){
+        if(id == val){
+            return;
+        }
+    }
+    friends_.insert(id);
+}   
 
-	//loop through friends_ set checking if the friend already exists
-	for(int val : friends_){
-		if(id == val){
-			return;
-		}
-	}
-
-	friends_.insert(id);
-
-	return;
-}	
-
+// Removes a friend by ID if they exist
 void User::deleteFriend(int id){
-
-	bool check = false;
-
-	//loops through friends_ set to check if the paramter id exists
-	for(int val : friends_){
-
-		if(val == id){
-			check = true;
-		}
-	}
-
-	if(check == true){
-		friends_.erase(id);
-	}
-
-	return;
+    if(friends_.count(id) > 0){
+        friends_.erase(id);
+    }
 }
 
-
+// Adds a post to the user's messages
 void User::addPost(Post* post){
-	messages_.push_back(post);
+    messages_.push_back(post);
 }
 
+// Adds a reaction to a specific post owned by the user
 void User::addReaction(int type, int postId, int userId){
-
-    Post* curPost = nullptr;
-
-
-	for(Post* i : messages_){
-		if(i->getMessageId() == postId){
-            curPost = i;
-            curPost->addReaction(type, userId);
-		}
-	}
+    for(Post* i : messages_){
+        if(i->getMessageId() == postId){
+            i->addReaction(type, userId);
+        }
+    }
 }
 
-
+// Returns all posts by the user
 std::vector<Post*> User::getPosts(){
-	return messages_;
+    return messages_;
 }
 
+// Returns a formatted string of the user's recent posts
+// Optionally filters to only show public posts
 std::string User::getPostsString(int howMany, bool showOnlyPublic){
-
     std::string result = "\n";
     int c = 1;
 
-	if (messages_.size() <= howMany){
-		for(Post* i : messages_){
-            result += std::to_string(c) + ". " + i->toString();
-			result += "\n\n";
+    if (messages_.size() <= howMany){
+        for(Post* i : messages_){
+            result += std::to_string(c) + ". " + i->toString() + "\n\n";
             c++;
-		}
-	}
-	else{
+        }
+    }
+    else{
         int index = messages_.size() - 5;
-		int count = 0;
+        int count = 0;
 
-//outputs the correct number of posts and only includes public posts if that is specified in the parameter
-		while(count < howMany){
-			if(!showOnlyPublic || messages_[index]->getIsPublic()){
-            result += std::to_string(c) + ". " + messages_[index]->toString();
-			result += "\n\n";
-			count++;}
+        while(count < howMany){
+            if(!showOnlyPublic || messages_[index]->getIsPublic()){
+                result += std::to_string(c) + ". " + messages_[index]->toString() + "\n\n";
+                count++;
+            }
             index++;
             c++;
-		}
+        }
+    }
 
-	}
-
-	return result;
+    return result;
 }
-
