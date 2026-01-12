2cc897e151e25ec219d4366068268248ffd00d7a
Quintus (Quin) Carlson <quintuscarlson@gmail.com>
2026-01-11 16:01:51 -0800

    Add comments to main function in main.cpp
    Added comments to clarify the main function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include "socialnetworkwindow.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    SocialNetworkWindow w;
-    w.show();
-    return a.exec();
+    QApplication a(argc, argv);  // Initialize the Qt application
+    SocialNetworkWindow w;       // Create the main application window
+    w.show();                    // Display the main window
+    return a.exec();             // Enter the Qt event loop
 }
